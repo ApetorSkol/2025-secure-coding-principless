6dd2ab95938a45ed8c826f5296de5dd5e01e3e71
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-03-19 14:17:34 +0100

    Integrating ArgParser into main

---
+++ src/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "ArgParser/ArgParser.h"
+
+
+int main(int argc, char *argv[]) {
+
+    ArgParser argParser(argc, argv);
+
+    try {
+        argParser.parse();
+    }
+    catch (const invalid_argument &ex) {
+        cerr << "[ERROR]: main: invalid_argument exception occured: " << ex.what() << endl;
+        return 1;
+    }
+    catch (const exception &ex) {
+        cerr << "[ERROR]: main: general exception occured" << endl;
+        return 1;
+    }
+
+
+    if (argParser.argExists("derive-key")) {
+        // doDeriveKeyThings(argParser.getArgValues(), argParser.returnFilepath());
+    }
+    else if (argParser.argExists("key-expression")) {
+        // doKeyExpressionThings(argParser.getArgValues(), argParser.returnFilepath());
+    }
+    else if (argParser.argExists("script-expression")) {
+        // doScriptExpressionThings(argParser.getArgValues(), argParser.getVerifyChecksumFlag, argParser.getComputeChecksumFlag());
+    }
+
+
+    return 0;
+}
+
